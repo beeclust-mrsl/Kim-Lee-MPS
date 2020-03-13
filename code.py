d0739a7beccba5cf7eb986e43e83ec16216d9173
Preshit <preshitx@gmail.com>
2020-03-13 17:45:10 +0530

    Added Eval and crossover functions,lists changed to numpy-arrays

--- code.py
+++ code.py
@@ -1,12 +1,15 @@
-#5 lines represented by their start pose and end pose
-lines=[
-		[ [x1,y1] , [xn1,yn1] ],
-		[ [x2,y2] , [xn2,yn2] ],		
-		[ [x3,y3] , [xn3,yn3] ],
-		[ [x4,y4] , [xn4,yn4] ],
-		[ [x5,y5] , [xn5,yn5] ]
+import numpy as np
+import math
 
-		]
+class GA:
+#5 lines represented by their start pose and end pose
+	lines=np.array([
+			[ [x1,y1] , [xn1,yn1] ],
+			[ [x2,y2] , [xn2,yn2] ],		
+			[ [x3,y3] , [xn3,yn3] ],
+			[ [x4,y4] , [xn4,yn4] ],
+			[ [x5,y5] , [xn5,yn5] ]
+			])
 
 #we have only 3 bots
 
@@ -16,54 +19,69 @@ lines=[
 #let initial pop has three chromosomes as array of three cost in pattern (1,2,3)  (2,3,4)   (3,4,5) according to 3 bots 
 
 
-group1[]=[ [ [x1,y1] , [xn1,yn1] ],
-			[ [x2,y2] , [xn2,yn2] ],		
-			[ [x3,y3] , [xn3,yn3] ], ]
+	group1[]=np.array([ lines[0]	, lines[1] , lines[2] ]	)	
+			
+	group2[]=np.array([ lines[1]	, lines[2] , lines[3] ] )
 
-group2[]=[ [ [x2,y2] , [xn2,yn2] ],
-			[ [x3,y3] , [xn3,yn3] ],		
-			[ [x4,y4] , [xn4,yn4] ], ]
-
-group3[]=[ [ [x3,y3] , [xn3,yn3] ],
-			[ [x4,y4] , [xn4,yn4] ],		
-			[ [x5,y5] , [xn5,yn5] ], ]
+	group3[]=np.array([ lines[2]	, lines[3] , lines[4] ] )
 
 
 
-def cal_cost(group[]):
-	totaldist=0
-	dist=0
-	startpose[]=[0,0]
-	endpose[]=[0,0]
-	#right now its 3 seperate lines ,will change it for connected lines
-	for i in range(0,3):
-		startpose[]=group[i][0]
-		endpose[]=group[i][1]
-		dist=math.sqrt( (endpose[0]-startpose[0])**2 + (endpose[1]-startpose[1])**2 )
-		totaldist=totaldist+dist
+	def cal_cost(group[]):
+		totaldist=0
+		dist=0
+		startpose[]=[0,0]
+		endpose[]=[0,0]
+	#right now its 3 seperate lines ,will change it for connected lines 
+		for i in range(0,3):
+			startpose[]=group[i][0]
+			endpose[]=group[i][1]
+			dist=math.sqrt( (endpose[0]-startpose[0])**2 + (endpose[1]-startpose[1])**2 )
+			totaldist=totaldist+dist
 
-	return totaldist
+		return totaldist
 
-
-
-
+#initial pop will be like pop=[cost1,cost2,cost3]
 
 
+#Evaluation (can find the pop with bestcost and hence bestpop....Fitness check)
+	def Eval(pop):
+
+		cost1=cal_cost(group1)
+		cost2=cal_cost(group2)
+		cost3=cal_cost(group3)
+		costs=[cost1,cost2,cost3]
+		cost=max(costs)
+		bestcost=cost
+		if (cost<bestcost):
+			bestcost=cost
+			bestpop=pop
+
+		return bestcost
+
+#Now apply genetic algorithm on this initial pop
+
+
+
+#GA
+
+
+	def crossover(parents,offspring_size):
+		offspring=np.empty(offspring_size)	#initialise 
+		crossover_pt=numpy.uint8(offspring_size[1]/2)
+
+		for k in range(offspring_size[0]):
+			parent1_id = k%parents.shape[0]
+			parent2_id = (k+1)%parents.shape[0]
+			offspring[k,0:crossover_pt]=parents[parent1_id,0:crossover_pt]
+			offspring[k,crossover_pt]=parents[parent2_id,crossover_pt:]
+
+		return offspring
+
+#adding variations to offspring using mutation
+	def mutation(offspring):
+		for id in range(offspring.shape[0]):
 
 
 
 
-
-
-
-
-
-
-
-
-def Fitness(inp,pop):
-	fitness=numpy.sum(pop*inp,axis=1)
-	return fitness
-
-def MatingSelect(pop,fitness,Num_prt):
-	parents
